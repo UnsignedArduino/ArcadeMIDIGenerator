642cb32b9ed6d601002d65d7897712d9122e0836
UnsignedArduino <UnsignedArduino@outlook.com>
2022-08-02 20:55:19 -0400

    Hide spam behind debug messages

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ parser = ArgumentParser(description="Turns a MIDI file into a lot of "
                                     "TypeScript code for MakeCode Arcade!")
 parser.add_argument("path", type=Path,
                     help="The path to the MIDI file. ")
-parser.add_argument("--output_path", type=Path,
-                    default=None, help="The path to write the code to")
-parser.add_argument("--stdout", action="store_const",
-                    const=True, default=False,
+parser.add_argument("--output_path", "-p", type=Path,
+                    default=None, help="The path to write the code to.")
+parser.add_argument("--stdout", "-s", action="store_true",
                     help="Whether to output everything to standard output "
-                         "instead of writing to a file")
+                         "instead of writing to a file. ")
+parser.add_argument("--debug", "-d", action="store_true",
+                    help="Whether to log debug messages or not. ")
 args = parser.parse_args()
 
 in_path = args.path.expanduser().resolve()
 out_path = args.output_path
+debug = args.debug
 to_stdout = args.stdout
 if out_path is None:
     # Put output file in the same directory as input file with same name
@@ -28,6 +30,11 @@ if out_path is None:
 
 
 def log(msg: str):
+    if not to_stdout and debug:
+        print(msg)
+
+
+def info(msg: str):
     if not to_stdout:
         print(msg)
 
@@ -36,7 +43,7 @@ def err(msg: str):
     stderr.write(msg)
 
 
-log(f"Parsing {in_path}")
+info(f"Parsing {in_path}")
 midi = MidiFile(in_path)
 
 if midi.type not in (0, 1):
@@ -103,6 +110,28 @@ def note_num_to_name(num: int) -> str:
     octave = ceil(num / 12)
     name = notes[num % 12]
     return name + str(octave)
+
+
+# https://gist.github.com/CGrassin/26a1fdf4fc5de788da9b376ff717516e
+# MIT License
+# Python to convert a string note (eg. "A4") to a frequency (eg. 440).
+# Inspired by https://gist.github.com/stuartmemo/3766449
+def get_frequency(note: str, A4: int = 440) -> float:
+    """
+    Get the frequency from a note name.
+
+    :param note: A note name, ex. "A4"
+    :param A4: The frequency of note A4. Defaults to 440. (hz)
+    :return: A float of the frequency.
+    """
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    octave = int(note[2]) if len(note) == 3 else int(note[1])
+    key_number = notes.index(note[0:-1])
+    if key_number < 3:
+        key_number = key_number + 12 + ((octave - 1) * 12) + 1
+    else:
+        key_number = key_number + ((octave - 1) * 12) + 1
+    return A4 * 2 ** ((key_number - 49) / 12)
 
 
 i = 0
